8d9f0bb3a27eb9ee92e78d654dc247f757c4a8a9
TashevYanislav <tashevyanislav@gmail.com>
2023-12-04 19:50:15 +0200

    paths object created

--- little-fashion/src/paths.js
+++ little-fashion/src/paths.js
@@ -1,4 +1,12 @@
 const Path = {
   Home: "/",
+  Logout: "/logout",
+  About: "/about-us",
+  Products: "/products",
+  Create: "/create",
+  Faq: "/faq",
+  Contact: "/contact",
+  Register: "/register",
+  Login: "/login",
 };
 export default Path;
