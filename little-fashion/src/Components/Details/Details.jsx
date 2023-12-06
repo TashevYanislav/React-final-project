0dcc95f40f2230aec2cbb5e555b8396869841e10
TashevYanislav <tashevyanislav@gmail.com>
2023-12-06 18:27:05 +0200

    edit and delete btn added

--- little-fashion/src/Components/Details/Details.jsx
+++ little-fashion/src/Components/Details/Details.jsx
@@ -1,17 +1,19 @@
-import { useContext, useEffect, useReducer, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useMemo, useReducer, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import * as productService from "../../services/productService";
 import * as CommentService from "../../services/commentService";
 import AuthContext from "../../contexts/authContext";
 import useForm from "../../hooks/useForm";
 import reducer from "./Commentreducer";
+import Path from "../../paths";
+
+import "./buttons.css";
 
 export default function Details() {
-  const { email } = useContext(AuthContext);
+  const { email, userId, isAuthenticated } = useContext(AuthContext);
   const [product, setProduct] = useState({});
   const [comments, dispatch] = useReducer(reducer, []);
   const { productId } = useParams();
-
 
   useEffect(() => {
     productService.getOne(productId).then(setProduct);
@@ -30,16 +32,27 @@ export default function Details() {
       values.commentText
     );
 
-    newComment.owner = {email} ;
+    newComment.owner = { email };
 
     dispatch({
       type: "ADD_COMMENT",
       payload: newComment,
     });
+
+    values.commentText = "";
   };
-  const { values, onChange, onSubmit } = useForm(addCommentHandler, {
-    commentText: "",
-  });
+
+  const initialValues = useMemo(
+    () => ({
+      commentText: "",
+    }),
+    []
+  );
+
+  const { values, onChange, onSubmit } = useForm(
+    addCommentHandler,
+    initialValues
+  );
 
   return (
     <main>
@@ -54,6 +67,23 @@ export default function Details() {
                   alt={product.name}
                 />
               </div>
+
+              {userId === product._ownerId && (
+                <div>
+                  <Link
+                    to={`/products/${productId}/edit`}
+                    className="button delete-btn"
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    to="/products/:productId/delete"
+                    className="button delete-btn"
+                  >
+                    Delete
+                  </Link>
+                </div>
+              )}
             </div>
             <div className="col-lg-6 col-12">
               <div className="product-info d-flex">
@@ -87,39 +117,44 @@ export default function Details() {
                     <option value={5}>5</option>
                   </select>
                 </div> */}
-                <div className="col-lg-6 col-12 mt-4 mt-lg-0">
-                  <button type="submit" className="btn custom-btn cart-btn">
-                    Add to Cart
-                  </button>
-                </div>
+                {isAuthenticated && (
+                  <div className="col-lg-6 col-12 mt-4 mt-lg-0">
+                    <button type="submit" className="btn custom-btn cart-btn">
+                      Add to Cart
+                    </button>
+                  </div>
+                )}
               </div>
-              {/* Add Comment */}
-              <div className="product-cart-thumb row">
-                <article className="col-lg-6 col-12 mt-4 mt-lg-0">
-                  <strong className="d-block mt-4 mb-2">Add Comment:</strong>
+              {isAuthenticated && (
+                <div className="product-cart-thumb row">
+                  <article className="col-lg-6 col-12 mt-4 mt-lg-0">
+                    <strong className="d-block mt-4 mb-2">Add Comment:</strong>
 
-                  <form onSubmit={onSubmit}>
-                    <div className="form-floating mb-4">
-                      <textarea
-                        type="text"
-                        name="commentText"
-                        value={values.commentText}
-                        onChange={onChange}
-                        id="commentText"
-                        className="form-control"
-                        placeholder="commentText"
-                        style={{ height: 100 }}
-                      />
-                      <label htmlFor="commentText">Comment...</label>
-                      <input
-                        type="submit"
-                        className="btn custom-btn cart-btn"
-                        value="Add Comment"
-                      />
-                    </div>
-                  </form>
-                </article>
-              </div>
+                    <form onSubmit={onSubmit}>
+                      <div className="form-floating mb-4">
+                        <textarea
+                          type="text"
+                          name="commentText"
+                          value={values.commentText}
+                          onChange={onChange}
+                          id="commentText"
+                          className="form-control"
+                          placeholder="commentText"
+                          style={{ height: 100 }}
+                        />
+                        <label htmlFor="commentText">Comment...</label>
+                        <input
+                          type="submit"
+                          className="btn custom-btn cart-btn"
+                          value="Add Comment"
+                        />
+                      </div>
+                    </form>
+                  </article>
+                </div>
+              )}
+              {/* Add Comment */}
+
               {/* Add Comment */}
             </div>
           </div>
@@ -130,13 +165,13 @@ export default function Details() {
             Customer <span>Comments</span>
           </h2>
         </div>
-        {comments.map(({ _id, text, owner:{email} }) => (
-            <div key={_id} className="slick-testimonial-caption">
-              <p className="lead">{text}</p>
-              <div className="slick-testimonial-client d-flex align-items-center mt-4">
-                <span>{email}</span>
-              </div>
+        {comments.map(({ _id, text, owner: { email } }) => (
+          <div key={_id} className="slick-testimonial-caption">
+            <p className="lead">{text}</p>
+            <div className="slick-testimonial-client d-flex align-items-center mt-4">
+              <span>{email}</span>
             </div>
+          </div>
         ))}
         {comments.length === 0 && <p>No comments yet...</p>}
 
